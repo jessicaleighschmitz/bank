64468eb4e38fd84f2ad1f24b0d7fb726f053ecd8
Guest User <Guest@Epicodus-10E.local>
2018-03-19 20:59:28 -0700

    add mroe logic

--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,49 @@ function BankAccount (name, amount) {
   this.amount = amount;
 }
 
-//BankAccount.protoype.profile = function () {
-//  return this.name + " has " + return this.amount + " available.";
-//}
-
-function resetFields () {
-  $("input#name").val("");
-  $("input#initial-deposit").val("");
-  $("input#deposit-amount").val("");
-  $("input#withdraw-amount").val("");
+BankAccount.prototype.fullUser = function(){
+  return this.name + ": $" + this.amount;
 }
+
+BankAccount.prototype.withdraw = function(withdraw) {
+  this.amount= this.amount - withdraw;
+}
+
+BankAccount.prototype.deposit = function() {
+  this.amount= this.amount + deposit;
+}
+
+// function resetFields () {
+//   $("input#name").val("");
+//   $("input#initial-deposit").val("");
+//   $("input#deposit-amount").val("");
+//   $("input#withdraw-amount").val("");
+// }
 
 $(document).ready(function() {
   $("form#new-account").submit(function(event) {
     event.preventDefault();
     var currentBalance = $("input#initial-deposit").val();
+    console.log(currentBalance);
+    var userName = $("input#name").val();
+    var newBankAccount = new BankAccount(userName, currentBalance);
+    $("#balance-container").append("<p>" + newBankAccount.fullUser() + "</p>")
 
-    $("#balance .output").append("$" + currentBalance);
+    console.log(newBankAccount);
+
+    // $("#balance-container .user-name").text(newBankAccount.name);
+    // $("#balance-container .user-balance").text(newBankAccount.amount);
 
 
+  $("#manage-account").click(function(event) {
+    event.preventDefault();
+
+    var withdraw = $("input#withdraw-amount").val();
+
+    var deposit = $("input#deposit-amount").val();
+    console.log(deposit);
+     $("balance-container").append("<p>" + newBankAccount.deposit() + "</p>");
+    //resetFields();
+    });
   });
 });
